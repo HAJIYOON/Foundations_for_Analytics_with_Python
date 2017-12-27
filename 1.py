b240b97cfb71d091807b67ef3d4bbf49f52c879b
HAJIYOON <34884719+HAJIYOON@users.noreply.github.com>
2017-12-27 19:21:44 +0900

    야호!

---
+++ 1.py
@@ -0,0 +1,9 @@
+﻿ print(“Hello!”) 
+   import turtle as t  #거북이 그래픽으로 도형 그리기
+   d = 100 #변수 이용하기
+   t.forward(d)
+   t.left(120)
+   print(“7*4 = “, 7*4) #수식 계산
+   for x in range(10): #for 명령어 사용하여 똑같은 작업 반복하기
+   print(“Hello!”) #반복할 내용 네 칸 띄고 입력(들여쓰기)
+   list(range(5)) #range(범위) 명령 사용 변화주며 반복하기 (p.52)
